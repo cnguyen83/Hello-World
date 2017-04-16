c2563dc7e3dd51ff9710d1845607345d52ea34a7
Christina Nguyen <cq_nguyen_vn@yahoo.com>
2017-04-16 10:10:42 -0600

    Add some more Elm practice files

--- hello_elm/hello-elm/die_roll.elm
+++ hello_elm/hello-elm/die_roll.elm
@@ -1,24 +1,53 @@
-import Html
-import String
+import Html exposing (..)
+import Html.Events exposing (..)
+import Random
 
+{- 
+    New to include "subscriptions" now since no longer using Html.beginnerProgram
+    {- Remember new architecture skeleton: https://guide.elm-lang.org/architecture/effects/ -}
+-}
+main = 
+    Html.program 
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 -- MODEL
-type alias model = 
+type alias Model = 
     { dieFace: Int }
 
+
 -- UPDATE
-type Msg = Roll
+type Msg = Roll 
+    | NewFace Int
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = 
     case msg of 
         Roll -> 
-            (model, Cmd.none)
+            (model, Random.generate NewFace (Random.int 1 6))
+
+        {- Step the model forward but don't do anything -}
+        NewFace newFace ->
+            (Model newFace, Cmd.none)
+
+-- SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 -- VIEW
 view : Model -> Html Msg
 view model = 
     div []
-        [ h1 [] [ text (toString model.dieFace)]
+        [ h1 [] [ text (toString model.dieFace) ]
         , button [ onClick Roll ] [ text "Roll" ]
         ]
+
+-- INIT 
+
+init : (Model, Cmd Msg)
+init = 
+    (Model 1, Cmd.none)
---
+++ hello_elm/hello-elm/giphy.elm
@@ -0,0 +1,30 @@
+import Html exposing (..)
+import Html.Events exposing(..)
+
+main = 
+    Html.program 
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+-- MODEL
+type alias Model = 
+    { topic: String
+    , gifUrl : String
+    }
+
+
+-- UPDATE 
+
+
+-- VIEW 
+
+
+-- SUBSCRIPTIONS
+
+
+-- INIT 
+
+
